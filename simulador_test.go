bd7b72d4c44285bef8dbd736438ab4bec711ee4b
Alessandro Raymundo <alessandroray@gmail.com>
2020-03-23 22:06:05 -0300

    Commit inical

---
+++ simulador_test.go
@@ -0,0 +1,25 @@
+package main_test
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/streadway/amqp"
+	"os"
+	"testing"
+)
+
+func Testinit(t *testing.T) {
+	err := godotenv.Load()
+	
+	if err != nil {
+		t.Errorf("Falha Carregar .env")
+	}
+}
+func TestConnect (t *testing.T) { 
+	dsn := "amqp://" + os.Getenv("RABBITMQ_DEFAULT_USER") + ":" + os.Getenv("RABBITMQ_DEFAULT_PASS") + "@" + os.Getenv("RABBITMQ_DEFAULT_HOST") + ":" + os.Getenv("RABBITMQ_DEFAULT_PORT") + os.Getenv("RABBITMQ_DEFAULT_VHOST")
+	_, err := amqp.Dial(dsn)
+
+	if err  != nil {
+		t.Error("Falha Conection")
+		return
+	}
+}
